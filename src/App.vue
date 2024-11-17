97145ac7fcc7750e0213b80db75df2f51adbad78
HaiPaya <haipaya@dzdgame.cn>
2024-11-17 10:17:02 +0800

    移除测试代码

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router"
 import {ref} from "vue"
-const a = ref(import.meta.env.VITE_API_URL)
+import { RouterLink, RouterView } from "vue-router"
 </script>
 
 <template>
-    <p>{{a}}</p>
 </template>
 
 <style scoped>
