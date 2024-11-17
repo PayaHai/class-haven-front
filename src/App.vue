043be227b25ab02ad6d07a4f568aec5ee853540d
HaiPaya <haipaya@dzdgame.cn>
2024-11-17 10:15:27 +0800

    测试环境变量是否生效

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router"
+import {ref} from "vue"
+const a = ref(import.meta.env.VITE_API_URL)
 </script>
 
 <template>
+    <p>{{a}}</p>
 </template>
 
 <style scoped>
